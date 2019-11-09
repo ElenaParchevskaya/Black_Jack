b2efbed41b52d2a35f48d2bd96ddd45c08390379
Elena Parchevskaya <elena.lemoon@gmail.com>
2019-11-09 19:15:23 +0300

    add class card, main, player, diller. added method create_deck_of_cards

---
+++ main.rb
@@ -0,0 +1,23 @@
+require_relative 'card'
+
+@cards = []
+
+#создаем колоду карт
+def create_deck_of_cards
+  suit = [9824, 9827, 9829, 9830]
+  4.times do |suit_number|
+    suit_str = "" << suit[suit_number]
+    2.upto(10) do |number|
+      name = number.to_s + suit_str
+      @cards << Card.new(name, number)
+    end
+    name = 'J' + suit_str
+    @cards << Card.new(name, 10)
+    name = 'Q' + suit_str
+    @cards << Card.new(name, 10)
+    name = 'K' + suit_str
+    @cards << Card.new(name, 10)
+    name = 'T' + suit_str
+    @cards << Card.new(name, 10)
+  end
+end
