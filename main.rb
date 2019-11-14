842faafcde4f156ed80288a88876014c42c82c3a
Elena Parchevskaya <elena.lemoon@gmail.com>
2019-11-14 10:55:57 +0300

    create main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 require_relative 'card'
 require_relative 'player'
 require_relative 'diller'
+require_relative 'module_card_options'
+
+include CardOptions
 
 RATE = -10
 
@@ -13,22 +16,7 @@ name = gets.chomp
 @player = Player.new(name)
 @diller = Diller.new
 
-#создаем колоду карт
-def create_deck_of_cards
-  suit = [9824, 9827, 9829, 9830]
-  4.times do |suit_number|
-    suit_str = "" << suit[suit_number]
-    2.upto(10) do |number|
-      name = number.to_s + suit_str
-      @cards << Card.new(name, number)
-    end
-    name = 'J' + suit_str
-    @cards << Card.new(name, 10)
-    name = 'Q' + suit_str
-    @cards << Card.new(name, 10)
-    name = 'K' + suit_str
-    @cards << Card.new(name, 10)
-    name = 'T' + suit_str
-    @cards << Card.new(name, 10)
-  end
-end
+create_deck_of_cards
+
+@bank -= 2 * RATE
+@player.change_bank(RATE)
